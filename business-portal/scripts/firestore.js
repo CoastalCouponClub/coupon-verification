9062141b7894715d4cbfb590b63e10d928956343
CoastalCouponClub <contact@coastalcouponclub.com>
2025-05-27 15:36:37 -0500

    Update firestore.js

--- business-portal/scripts/firestore.js
+++ business-portal/scripts/firestore.js
@@ -1,1 +1,11 @@
-// Placeholder for firestore interaction logic
+// Placeholconst firebaseConfig = {
+  apiKey: "AIzaSyBJxxcGhuYspiZ9HRAIZgihgXLaA2FjPXc",
+  authDomain: "coastalcouponverifier.firebaseapp.com",
+  projectId: "coastalcouponverifier",
+  storageBucket: "coastalcouponverifier.appspot.com",
+  messagingSenderId: "189807704712",
+  appId: "1:189807704712:web:9427e68464115f388ebd3d"
+};
+
+export default firebaseConfig;
+der for firestore interaction logic
